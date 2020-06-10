73a0fd86fca39192118f7c4d0a9c1b797e3054c6
EdsonMSouza <prof.edson.melo@gmail.com>
2020-06-10 14:15:01 -0300

    Webservice API

--- aula_2905_api/app/src/main/java/com/ems/aula_2905_api/MainActivity.java
+++ aula_2905_api/app/src/main/java/com/ems/aula_2905_api/MainActivity.java
@@ -1,7 +1,12 @@
 package com.ems.aula_2905_api;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,14 +26,20 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-    StringBuilder response = new StringBuilder();
     String jsonInputString = null;
+    EditText loginUsuario, loginSenha, txtNome, txtUsuario, txtSenha;
+    Button btCadastrar, btLogin;
+    TextView saida;
+
+    // Cria uma variável para armazenar os dados internamente no Android
+    SharedPreferences sharedPreferences;
 
     public class UserService extends AsyncTask<Void, Void, List<User>> {
         // construtor da classe UserService
         public UserService(User user) {
             // cria uma instância do objeto Gson
             Gson gson = new Gson();
+            // preparando a string para ser enviada como JSON (Parseada)
             jsonInputString = gson.toJson(user);
         }
 
@@ -38,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected List<User> doInBackground(Void... voids) {
+            final StringBuilder response = new StringBuilder();
             try {
                 // instanciar os objetos para se conectar na internet
                 // na variável url informamos o nosso endPoint
@@ -57,16 +69,17 @@ public class MainActivity extends AppCompatActivity {
                 try (OutputStream os = serv.getOutputStream()) {
                     // informando para requisição o tipo de dados a ser enviado
                     byte[] input = jsonInputString.getBytes("utf-8");
-                    // enviar os dados
+                    // enviar os dados (de fato)
                     os.write(input, 0, input.length);
                 }
 
-                // ler a resposta do servidor
+                // ler a resposta do servidor (Response)
                 try (BufferedReader br = new BufferedReader(
                         new InputStreamReader(serv.getInputStream(), "utf-8"))) {
 
                     // variável para receber as linhas enviadas pelo servidor
                     String responseLine = null;
+                    //response.delete(0, response.indexOf(""));
 
                     // percorre o retorno do servidor e coloca na variável
                     while ((responseLine = br.readLine()) != null) {
@@ -77,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             } catch (IOException ex) {
-
             }
 
             // Criação de um tipo para receber os dados
@@ -94,28 +106,101 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        User user = new User();
-        user.setType("login");
-        user.setUser("edson.melo");
-        user.setPassword("edson123");
+        sharedPreferences = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
 
-        TextView resultado = findViewById(R.id.resultado);
+        // Eliminar os dados das Preferences
+        // SharedPreferences.Editor myVars = sharedPreferences.edit();
+        // myVars.clear();
+        // myVars.commit();
 
-        // chamar o serviço HTTP para API
-        try {
-            ArrayList<User> alUser = (ArrayList<User>) new UserService(user).execute().get();
-            // percorrendo o objeto resultante para ler o conteúdo dele (Atributos)
-            for (User u : alUser) {
-                resultado.setText(
-                        u.getId() + "\n" +
-                                u.getName() + "\n" +
-                                u.getPassword()
-                );
-            }
-        } catch (ExecutionException e) {
-        } catch (InterruptedException e) {
+        // Associando os campos de login
+        loginUsuario = findViewById(R.id.txtUsuario);
+        loginSenha = findViewById(R.id.txtSenha);
+        saida = findViewById(R.id.txtSaida);
+        btLogin = findViewById(R.id.btLogin);
+
+        // Associando campos do cadastro
+        txtNome = findViewById(R.id.editTextNome);
+        txtUsuario = findViewById(R.id.editTextUsuario);
+        txtSenha = findViewById(R.id.editTextSenha);
+        btCadastrar = findViewById(R.id.btCadastrar);
+
+        // vou usar os mesmos campos para mostrar os dados
+        if(sharedPreferences.contains("user")) {
+            loginUsuario.setText(sharedPreferences.getString("user", "") );
+            loginSenha.setText(sharedPreferences.getString("password", "") );
         }
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // cria o objeto Usuario (user)
+                User user = new User();
+                // {"type":"login", "user":"tio.patinhas", "password":"123"}
+
+                user.setType("login"); // tipo de operação na API
+                user.setUser(loginUsuario.getText().toString());
+                user.setPassword(loginSenha.getText().toString());
+
+                // chamar o serviço HTTP para API
+                try {
+                    ArrayList<User> alUser = (ArrayList<User>)
+                            new UserService(user).execute().get();
+
+                    // percorrendo o objeto resultante para ler o conteúdo dele (Atributos)
+                    for (User u : alUser) {
+                        if(u.getType() == null){
+                            saida.setText("Olá, " + u.getName());
+
+                            // Cria as entradas para as preferências
+                            SharedPreferences.Editor myVars = sharedPreferences.edit();
+                            myVars.putString("user", u.getUser());
+                            myVars.putString("password", u.getPassword());
+                            myVars.commit();
+
+                        } else{
+                            if (u.getType().equals("request_incorreto")) {
+                                saida.setText("Preencha os campos");
+                            } else if (u.getType().equals("login_incorreto")) {
+                                saida.setText("Usuário/senha incorretos");
+                            }
+                        }
+                    }
+                } catch (ExecutionException e) {
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // cria o objeto Usuario (user)
+                User user = new User();
+                // {"type":"new", "name":"um nome qualquer", user":"tio.patinhas", "password":"123"}
+
+                user.setType("new"); // tipo de operação na API
+                user.setName(txtNome.getText().toString());
+                user.setUser(txtUsuario.getText().toString());
+                user.setPassword(txtSenha.getText().toString());
+
+                // chamar o serviço HTTP para API
+                try {
+                    ArrayList<User> alUser = (ArrayList<User>)
+                            new UserService(user).execute().get();
+                    // percorrendo o objeto resultante para ler o conteúdo dele (Atributos)
+                    for (User u : alUser) {
+                        if(u.getType().equals("true")){
+                            saida.setText("Olá, " + u.getName());
+                        } else{
+                            saida.setText("Erro ao criar o usuário");
+                        }
+                    }
+                } catch (ExecutionException e) {
+                } catch (InterruptedException e) {
+                }
+            }
+        });
     }
-    //new UserService(user).execute(); // chama o serviço para a API
 }
 
